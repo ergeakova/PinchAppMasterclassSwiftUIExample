ec762940df1a3c0badbb20c5ee6e6a95b3ea5e05
ergeakova <ergeak@gmail.com>
2022-09-08 02:24:37 +0300

    Initial Commit

---
+++ PinchAppMasterclassSwiftUIExample/PinchAppMasterclassSwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchAppMasterclassSwiftUIExampleApp.swift
+//  PinchAppMasterclassSwiftUIExample
+//
+//  Created by Erge Gevher Akova on 8.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct PinchAppMasterclassSwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
